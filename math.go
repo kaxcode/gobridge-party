fd02f48ba2599adc95a4e5d32076e924373f2e88
Kenia <keniavalladarez@gmail.com>
2018-04-23 15:03:59 -0400

    Hello, gobridge-party
    Add basic math challenges and tests.

---
+++ math.go
@@ -0,0 +1,15 @@
+package main
+
+func Sum(x int, y int) int {
+	return x + y
+}
+
+// Fix Multiply Function
+func Multiply(a, b int) int {
+	return a *  b
+}
+
+// Write func for Subtraction
+
+
+// Write func for Division
---
+++ math_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+    total := Sum(5, 5)
+    if total != 10 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+    }
+}
+
+func TestMultiply(t *testing.T) {
+	total := Multiply(5, 5)
+	if total != 25 {
+		 t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+}
+
+func TestSubtraction(t *testing.T) {
+	total := Subtraction(37, 5)
+	if total != 32 {
+		 t.Errorf("Subtraction was incorrect, got: %d, want: %d.", total, 32)
+	}
+}
+
+func TestDivision(t *testing.T) {
+	total := Division(30, 6)
+	if total != 5 {
+		 t.Errorf("Divison was incorrect, got: %d, want: %d.", total, 5)
+	}
+}
