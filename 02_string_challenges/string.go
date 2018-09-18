1116288b017e98c25b8c37a8b2522f79e326eabf
Kenia <keniavalladarez@gmail.com>
2018-09-17 22:54:08 -0400

    Updates folder numbering.

--- 02_string_challenges/string.go
+++ 02_string_challenges/string.go
@@ -4,11 +4,11 @@ import (
 	"strings"
 )
 
-func StringRpt(value string, repetitions int) string {
+func stringRpt(value string, repetitions int) string {
   return strings.Repeat(value, repetitions)
 }
 
-func StringHasPrefix(value string, prefix string) bool {
+func stringHasPrefix(value string, prefix string) bool {
 	// Write the return
 }
 
