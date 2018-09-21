6ebab894cd6853c10dbc43d97bd3ed8980a0df5d
Kenia <keniavalladarez@gmail.com>
2018-09-21 14:59:03 -0400

    Add final challenge.

---
+++ 04_final_challenge/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// read the proverbs file // use ioutil package
+
+	// transform bytes into string
+
+	// print out lines of the each line as a string with a count of each character int the string
+	// output results
+	// Documentation is for users. map[ :3 r:2:1 o:3 c:1 i:2 s:3 f:1 e:2 n:2 t:2 a:1 D:1 u:2 .:1 m:1]
+  // Don't panic. map[a:1 c:1 ':1  :1 p:1 n:2 t:1 i:1 .:1:1 D:1 o:1]
+}
